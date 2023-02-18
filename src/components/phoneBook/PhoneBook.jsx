85efb62c5d0f57a4c9f3abab22b0476485622879
kolac300 <kolac300@gmail.com>
2023-02-18 21:20:24 +0200

    redux native version

---
+++ src/components/phoneBook/PhoneBook.jsx
@@ -0,0 +1,76 @@
+import { Label, StyledForm, InvalidValue } from './PhoneBook.styled';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'Redux/actions';
+import { toast } from 'react-hot-toast';
+import { getContacts } from 'Redux/selector';
+
+const phoneRegexp = /^\+380\d{3}\d{2}\d{2}\d{2}$/;
+const scheme = yup.object().shape({
+  number: yup
+    .string()
+    .trim()
+    .matches(phoneRegexp, 'add correct format phone +380ххххххххх')
+    .required(),
+  name: yup.string().min(3).max(40).required(),
+});
+const initialValues = {
+  number: '',
+  name: '',
+};
+
+export const PhoneBook = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts)
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts])
+
+
+  const isAlredyExistInContact = (name) => {
+    return contacts
+      .some(contact =>
+        contact.name.toLowerCase()
+        === name.toLowerCase())
+  }
+
+  const onSubmit = (value, { resetForm }) => {
+    const { name, number } = value;
+    if (isAlredyExistInContact(name))
+      toast.error("Contact alredy exists")
+    else {
+      dispatch(addContact(name, number))
+      resetForm();
+      toast.success('Successfully added!')
+    }
+  };
+
+  return (
+    <>
+      <h1>Phone Book</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={scheme}
+        onSubmit={onSubmit}
+      >
+        <StyledForm>
+          <Label>Name</Label>
+          <Field type="text" name="name" />
+          <ErrorMessage component={InvalidValue} name="name" />
+          <Label>Number</Label>
+          <Field type="tel" name="number" />
+          <ErrorMessage component={InvalidValue} name="number" />
+          <Label htmlFor="">
+            <button type="submit">Add contact</button>
+          </Label>
+        </StyledForm>
+      </Formik>
+    </>
+  );
+};
+// ContactForm.propTypes = {
+//   onAddContact: PropTypes.func.isRequired,
+// };
